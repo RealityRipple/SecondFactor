06f573fec4a11c2de907cd1eb6ed41363ad61a31
Andy <webmaster@RealityRipple.com>
2025-01-07 16:55:34 -0800

    Manual Single Instancing using Mutex and Pipe
    .NET's built-in SingleInstance system seems to have major issues.

--- SecondFactor/ApplicationEvents.vb
+++ SecondFactor/ApplicationEvents.vb
@@ -1,13 +1,14 @@
-﻿Imports Microsoft.VisualBasic.ApplicationServices
-Namespace My
-  ' The following events are available for MyApplication:
-  ' Startup: Raised when the application starts, before the startup form is created.
-  ' Shutdown: Raised after all application forms are closed.  This event is not raised if the application terminates abnormally.
-  ' UnhandledException: Raised if the application encounters an unhandled exception.
-  ' StartupNextInstance: Raised when launching a single-instance application and the application is already active. 
-  ' NetworkAvailabilityChanged: Raised when the network connection is connected or disconnected.
+﻿Namespace My
   Partial Friend Class MyApplication
-    Private Sub MyApplication_Startup(ByVal sender As Object, ByVal e As StartupEventArgs) Handles Me.Startup
+    Private WithEvents sia As SingleInstance
+    Private Sub MyApplication_Startup(ByVal sender As Object, ByVal e As ApplicationServices.StartupEventArgs) Handles Me.Startup
+      sia = New SingleInstance({frmMain.Name, frmPassEntry.Name})
+      If Not sia.NewMutex() Then
+        sia.Send(e.CommandLine)
+        e.Cancel = True
+        Return
+      End If
+      sia.NewPipe()
       Dim v As Authenticode.Validity = Authenticode.IsSelfSigned(Reflection.Assembly.GetExecutingAssembly().Location)
       If Not (v = Authenticode.Validity.SignedAndValid Or v = Authenticode.Validity.SignedButUntrusted) Then
         Dim sErr As String = "0x" & v.ToString("x")
@@ -83,7 +84,7 @@ Namespace My
       End If
       If My.Computer.Registry.ClassesRoot.GetSubKeyNames.Contains("otpauth") Then My.Computer.Registry.ClassesRoot.DeleteSubKeyTree("otpauth")
     End Sub
-    Private Sub MyApplication_StartupNextInstance(ByVal sender As Object, ByVal e As Microsoft.VisualBasic.ApplicationServices.StartupNextInstanceEventArgs) Handles Me.StartupNextInstance
+    Private Sub sia_NewInstanceStartup(ByVal sender As Object, ByVal e As Microsoft.VisualBasic.ApplicationServices.StartupNextInstanceEventArgs) Handles sia.NewInstanceStartup
       Dim sImport As String = Nothing
       If e.CommandLine IsNot Nothing AndAlso e.CommandLine.Count > 0 Then
         Dim sCmd As String = Join(e.CommandLine.ToArray)
@@ -99,14 +100,105 @@ Namespace My
           sImport = sCmd.Substring(8)
         End If
       End If
-      If Not String.IsNullOrEmpty(sImport) Then
-        If frmMain.Visible Then
-          e.BringToForeground = True
-          frmMain.ParseOTPURL(sImport, False)
-        Else
-          MsgBox("Failed to import new Authenticator Profile. You must successfully log in before adding a new profile.", MsgBoxStyle.Critical, My.Application.Info.ProductName)
-        End If
+      If String.IsNullOrEmpty(sImport) Then
+        e.BringToForeground = True
+        Return
       End If
+      Dim fMain As frmMain = Nothing
+      For Each f As Form In Application.OpenForms
+        If Not f.Name = frmMain.Name Then Continue For
+        fMain = f
+        Exit For
+      Next
+      If fMain IsNot Nothing AndAlso fMain.Visible Then
+        e.BringToForeground = True
+        fMain.ParseOTPURL(sImport, False)
+      Else
+        MsgBox("Failed to import new Authenticator Profile. You must successfully log in before adding a new profile.", MsgBoxStyle.Critical, My.Application.Info.ProductName)
+      End If
+    End Sub
+  End Class
+  Friend Class SingleInstance
+    Public Event NewInstanceStartup(ByVal sender As Object, ByVal e As Microsoft.VisualBasic.ApplicationServices.StartupNextInstanceEventArgs)
+    Private mutex As Threading.Mutex
+    Private pipe As IO.Pipes.NamedPipeServerStream
+    Private MutexID As String = Nothing
+    Private FocusForms As String() = Nothing
+    Public Sub New(ByVal sFocusList As String())
+      mutex = Nothing
+      pipe = Nothing
+      MutexID = System.Runtime.InteropServices.Marshal.GetTypeLibGuidForAssembly(System.Reflection.Assembly.GetExecutingAssembly).ToString("D")
+      FocusForms = sFocusList
+    End Sub
+    Public Function NewMutex() As Boolean
+      Try
+        Dim isNew As Boolean = True
+        mutex = New Threading.Mutex(True, MutexID, isNew)
+        Return isNew
+      Catch ex As Exception
+        Return False
+      End Try
+    End Function
+    Public Function NewPipe() As Boolean
+      Try
+        If pipe IsNot Nothing Then
+          pipe.Close()
+          pipe.Dispose()
+          pipe = Nothing
+        End If
+        pipe = New IO.Pipes.NamedPipeServerStream(MutexID, IO.Pipes.PipeDirection.In, IO.Pipes.NamedPipeServerStream.MaxAllowedServerInstances, IO.Pipes.PipeTransmissionMode.Message, IO.Pipes.PipeOptions.Asynchronous Or IO.Pipes.PipeOptions.WriteThrough)
+        pipe.BeginWaitForConnection(AddressOf Connected, Nothing)
+        Return True
+      Catch ex As Exception
+        Return False
+      End Try
+    End Function
+    Public Function Send(ByVal CommandLine As Collections.ObjectModel.ReadOnlyCollection(Of String)) As Boolean
+      Try
+        Using sendPipe As New IO.Pipes.NamedPipeClientStream(".", MutexID, IO.Pipes.PipeDirection.Out, IO.Pipes.PipeOptions.Asynchronous Or IO.Pipes.PipeOptions.WriteThrough, Security.Principal.TokenImpersonationLevel.Anonymous)
+          sendPipe.Connect(2000)
+          If Not sendPipe.IsConnected Then Return False
+          Using wStream As New IO.StreamWriter(sendPipe)
+            wStream.WriteLine(My.Application.Info.ProductName & " - " & MutexID)
+            wStream.WriteLine(CommandLine.Count)
+            For Each n As String In CommandLine
+              wStream.WriteLine(n)
+            Next
+          End Using
+          sendPipe.Close()
+          Return True
+        End Using
+      Catch ex As Exception
+        Return False
+      End Try
+    End Function
+    Private Sub Connected(ByVal ar As IAsyncResult)
+      Try
+        pipe.EndWaitForConnection(ar)
+        Dim CommandLine As New List(Of String)
+        Using rStream As New IO.StreamReader(pipe)
+          If Not rStream.ReadLine() = My.Application.Info.ProductName & " - " & MutexID Then Return
+          Dim sLen As String = rStream.ReadLine()
+          Dim iLen As Integer = CInt(sLen)
+          Do Until rStream.EndOfStream
+            CommandLine.Add(rStream.ReadLine())
+          Loop
+          If Not iLen = CommandLine.Count Then CommandLine = New List(Of String)
+        End Using
+        NewPipe()
+        Dim e As New Microsoft.VisualBasic.ApplicationServices.StartupNextInstanceEventArgs(CommandLine.AsReadOnly, False)
+        RaiseEvent NewInstanceStartup(Me, e)
+        If e.BringToForeground Then
+          For Each frm As Form In Application.OpenForms
+            If Not FocusForms.Contains(frm.Name) Then Continue For
+            If Not frm.Visible Then frm.Show()
+            frm.BringToFront()
+            frm.Activate()
+            Exit For
+          Next
+        End If
+      Catch ex As Exception
+      End Try
     End Sub
   End Class
 End Namespace
